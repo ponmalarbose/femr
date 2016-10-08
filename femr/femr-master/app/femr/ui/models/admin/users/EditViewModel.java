239af939d16e24ca64c6099331ca82225bcec107
ponmalarbose <ponmalar95.bose@gmail.com>
2016-10-08 13:51:37 -0500

    updated femr-change req 1

--- femr/femr-master/app/femr/ui/models/admin/users/EditViewModel.java
+++ femr/femr-master/app/femr/ui/models/admin/users/EditViewModel.java
@@ -48,9 +48,8 @@ public class EditViewModel {
             errors.add(new ValidationError("email", "email is a required field"));
         if (!newPassword.equals(newPasswordVerify))
             errors.add(new ValidationError("newPassword", "passwords do not match"));
-        else if(newPassword.isEmpty() || newPasswordVerify.isEmpty())
-            errors.add(new ValidationError("newPassword", "password field is empty"));
-        else {
+       if(newPassword.length() > 0)
+        {
             if(newPassword.length() < 6 || !hasUppercase.matcher(newPassword).find()
                     || !hasNumber.matcher(newPassword).find())      //AJ Saclayan Password Constraints
                 errors.add(new ValidationError("newPassword",
