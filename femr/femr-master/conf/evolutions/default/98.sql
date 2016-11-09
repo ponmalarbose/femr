252e8107a086e57369d2b6eb0b5fc4cbe2092086
ponmalarbose <ponmalar95.bose@gmail.com>
2016-11-08 20:42:02 -0600

    Revert "femr-master"
    This reverts commit 42a9a2b1315d79aeaae298733de445fa245a4265.

--- femr/femr-master/conf/evolutions/default/98.sql
+++
@@ -1,9 +0,0 @@
-# --- !Ups
-
-ALTER TABLE `patient_encounters`
-ADD COLUMN `is_diabetes_screened` BOOL NULL DEFAULT NULL AFTER `mission_trip_id`;
-
-# --- !Downs
-
-ALTER TABLE `patient_encounters`
-DROP COLUMN `is_diabetes_screened`;
